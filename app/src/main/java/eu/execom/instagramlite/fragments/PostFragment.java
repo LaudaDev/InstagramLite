2436ff357c775961b42aef439e48c09fe053415a
Cobrijani <cobrijani@gmail.com>
2016-04-20 16:00:06 +0200

    renamed homefragment to postfragment

--- app/src/main/java/eu/execom/instagramlite/fragments/PostFragment.java
+++ app/src/main/java/eu/execom/instagramlite/fragments/PostFragment.java
@@ -2,10 +2,8 @@ package eu.execom.instagramlite.fragments;
 
 import android.support.v4.app.Fragment;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.UiThread;
@@ -14,8 +12,8 @@ import org.androidannotations.annotations.ViewById;
 import eu.execom.instagramlite.R;
 import eu.execom.instagramlite.adapters.UserPostsAdapter;
 
-@EFragment(R.layout.fragment_home)
-public class HomeFragment extends Fragment {
+@EFragment(R.layout.fragment_post)
+public class PostFragment extends Fragment {
 
     @ViewById
     ListView listView;
