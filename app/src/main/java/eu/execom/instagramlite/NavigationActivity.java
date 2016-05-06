fd13ce63ed6375ea76b6b8ea51596fe4f6ac3264
Nikola Lukić <nlukic@execom.eu>
2016-05-06 09:11:48 +0200

    image capture working

--- app/src/main/java/eu/execom/instagramlite/NavigationActivity.java
+++ app/src/main/java/eu/execom/instagramlite/NavigationActivity.java
@@ -1,19 +1,32 @@
 package eu.execom.instagramlite;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.OnActivityResult;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import eu.execom.instagramlite.adapters.TabAdapter;
 import eu.execom.instagramlite.repository.UserRepository;
@@ -22,7 +35,15 @@ import eu.execom.instagramlite.repository.UserRepository;
 @OptionsMenu(R.menu.menu_options_navigation)
 public class NavigationActivity extends AppCompatActivity {
 
+    private static final String TAG = NavigationActivity.class.getSimpleName();
+
+    private static final String PHOTO_PATH = "PHOTO_PATH";
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     boolean doubleBackToExitPressedOnce = false;
+
+    String currentPhotoPath;
 
     @Extra
     String username;
@@ -76,6 +97,68 @@ public class NavigationActivity extends AppCompatActivity {
     void itemLogout() {
         userRepository.invalidateSession();
         finish();
+    }
+
+    @Click
+    void newPost() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @OnActivityResult(REQUEST_IMAGE_CAPTURE)
+    void onResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Toast.makeText(this, currentPhotoPath, Toast.LENGTH_LONG).show();
+            Log.d("paht", currentPhotoPath);
+
+            NewPostActivity_.intent(this).imagePath(currentPhotoPath).start();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        final String timeStamp = String.valueOf(System.currentTimeMillis());
+        final String imageFileName = "JPEG_" + timeStamp + "_";
+        Log.d("imageFileName", imageFileName);
+        final File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        final File instagramLiteStorageDir = new File(String.format("%s/InstagramLite", storageDir.getAbsolutePath()));
+        instagramLiteStorageDir.mkdir();
+        final File image = File.createTempFile(
+                imageFileName,           /* prefix    */
+                ".jpg",                  /* suffix    */
+                instagramLiteStorageDir  /* directory */
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        Log.d(TAG, currentPhotoPath);
+        return image;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putString(PHOTO_PATH, currentPhotoPath);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            currentPhotoPath = savedInstanceState.getString(PHOTO_PATH);
+        }
     }
 
 }
---
+++ app/src/main/java/eu/execom/instagramlite/NewPostActivity.java
@@ -0,0 +1,42 @@
+package eu.execom.instagramlite;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.common.util.UriUtil;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.EditorAction;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_new_post)
+public class NewPostActivity extends AppCompatActivity {
+
+    @Extra
+    String imagePath;
+
+    @ViewById
+    SimpleDraweeView image;
+
+    @ViewById
+    EditText description;
+
+    @AfterViews
+    void setImage() {
+        Uri uri = new Uri.Builder().scheme(UriUtil.LOCAL_FILE_SCHEME).path(imagePath).build();
+        image.setImageURI(uri);
+    }
+
+    @Click
+    @EditorAction(R.id.description)
+    void submitPost() {
+        Toast.makeText(this, description.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+}
